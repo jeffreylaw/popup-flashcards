38e9f7a50adde1b2e3422e3925dbb9826b88d4e1
Jeffrey Law <jylday56@gmail.com>
2022-11-03 00:12:39 -0700

    Add Flip animation for cards in popup, add styling for popup

--- popup/popup.js
+++ popup/popup.js
@@ -8,13 +8,23 @@ chrome.storage.local.get(null, (items) => {
         }, {});
 
     let cardDiv = document.getElementById("card-div");
-    let skipBtn = document.getElementById("skip-btn");
+    let nextBtn = document.getElementById("next-btn");
     let disableBtn = document.getElementById("disable-btn");
     let enableBtn = document.getElementById("enable-btn");
-    let myCards = document.getElementById("my-cards");
-    let help = document.getElementById("help");
+    let myCards = document.getElementById("cards-btn");
     let questionContainer = document.getElementById("question-container");
     let answerContainer = document.getElementById("answer-container");
+    let frequencyInput = document.getElementById("frequency-slider-input")
+    let frequencyInputDisplay = document.getElementById("frequency-value-display");
+    let helpBtn = document.getElementById("help-btn");
+    let extFrequencyProp = "EXTENSION_SETTING_FREQUENCY_" + chrome.runtime.id;
+
+    if (items.extFrequencyProp) {
+        frequencyInputDisplay.textContent = "Every " + items[extFrequencyProp] + " min";
+    } else {
+        frequencyInputDisplay.textContent = "Every " + items[extFrequencyProp] + " mins";
+    }
+    frequencyInput.value = items[extFrequencyProp];
 
 
     if (items["EXTENSION_SETTING_ENABLED_" + chrome.runtime.id]) {
@@ -28,16 +38,15 @@ chrome.storage.local.get(null, (items) => {
     showNewCard(cards);
 
     cardDiv.addEventListener("click", function () {
-        if (questionContainer.className === "") {
-            questionContainer.className = "hide";
-            answerContainer.className = "";
-        } else {
-            questionContainer.className = "";
-            answerContainer.className = "hide";
-        }
+        let cardDivInner = document.getElementById("card-div-inner");
+        cardDivInner.classList.toggle("is-flipped");
     });
 
-    skipBtn.addEventListener("click", function () {
+    nextBtn.addEventListener("click", function () {
+        let cardDivInner = document.getElementById("card-div-inner");
+        if (cardDivInner.className === "is-flipped") {
+            cardDivInner.classList.toggle("is-flipped");
+        }
         showNewCard();
     });
 
@@ -47,26 +56,20 @@ chrome.storage.local.get(null, (items) => {
         });
     });
 
-    help.addEventListener("click", function () {
-        chrome.tabs.create({
-            url: chrome.runtime.getURL("help-page/help.html")
-        });
-    });
-
     enableBtn.addEventListener("click", function () {
         let extSettingProp = "EXTENSION_SETTING_ENABLED_" + chrome.runtime.id;
         let propObject = {
             [extSettingProp]: true
         };
-        chrome.tabs.query({}, function(tabs) {
-            for (let i=0; i<tabs.length; i++) {
+        chrome.tabs.query({}, function (tabs) {
+            for (let i = 0; i < tabs.length; i++) {
                 console.log(tabs[i])
                 if (!tabs[i].url.startsWith("http") || !tabs[i].url.startsWith("https")) {
                     continue;
                 }
                 chrome.tabs.sendMessage(tabs[i].id, propObject);
             }
-          });
+        });
 
         chrome.storage.local.set(propObject);
         setEnableDisableBtn("enable");
@@ -77,17 +80,30 @@ chrome.storage.local.get(null, (items) => {
         let propObject = {
             [extSettingProp]: false
         };
-        chrome.tabs.query({}, function(tabs) {
-            for (let i=0; i<tabs.length; i++) {
+        chrome.tabs.query({}, function (tabs) {
+            for (let i = 0; i < tabs.length; i++) {
                 console.log(tabs[i])
                 if (!tabs[i].url.startsWith("http") || !tabs[i].url.startsWith("https")) {
                     continue;
                 }
                 chrome.tabs.sendMessage(tabs[i].id, propObject);
             }
-          });
+        });
         chrome.storage.local.set(propObject);
         setEnableDisableBtn("disable");
+    });
+
+    frequencyInput.addEventListener("change", function(e) {
+        let extSettingProp = "EXTENSION_SETTING_FREQUENCY_" + chrome.runtime.id;
+        let propObject = {
+            [extSettingProp]: parseInt(e.target.value)
+        };
+        if (e.target.value === "1") {
+            frequencyInputDisplay.textContent = "Every " + e.target.value + " min";
+        } else {
+            frequencyInputDisplay.textContent = "Every " + e.target.value + " mins";
+        }
+        chrome.storage.local.set(propObject);
     });
 
     function setEnableDisableBtn(status) {
@@ -102,13 +118,20 @@ chrome.storage.local.get(null, (items) => {
 
     function showNewCard() {
         let randomQnA = getRandomQuestionAndAnswer(cards);
+        if (questionContainer.textContent) {
+            while (questionContainer.textContent === randomQnA[0]) {
+                randomQnA = getRandomQuestionAndAnswer(cards);
+            }
+        }
+        console.log(randomQnA);
         questionContainer.textContent = randomQnA[0];
         answerContainer.textContent = randomQnA[1];
-        questionContainer.className = "";
-        answerContainer.className = "hide";
     }
 });
 
+function updateFrequencyValue(value) {
+    document.getElementById("frequency-value-display").textContent = value;
+}
 
 function getRandomQuestionAndAnswer(cards) {
     let randInt = Math.floor(Math.random() * Object.getOwnPropertyNames(cards).length);
